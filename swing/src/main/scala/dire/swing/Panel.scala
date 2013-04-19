d02c9c9f0519113a9b8e8bf7e5e5b2d9d6f3fa51
stefan-hoeck <efasckenoth@gmail.com>
2013-04-19 05:47:30 +0200

    fixed swing.Panel so that peer can be accessed from client code

--- swing/src/main/scala/dire/swing/Panel.scala
+++ swing/src/main/scala/dire/swing/Panel.scala
@@ -4,9 +4,7 @@ import java.awt.{GridBagLayout, Dimension}
 import javax.swing.JPanel
 import scalaz.effect.IO
 
-class Panel private(private[swing] val peer: JPanel) {
-
-}
+class Panel private(val peer: JPanel) extends Component[JPanel]
 
 object Panel {
   def apply(): IO[Panel] = IO(new JPanel) flatMap apply
