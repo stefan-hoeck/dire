2463150663e3ced538645733583961734e648451
Stefan Hoeck <hock@zhaw.ch>
2013-04-19 14:11:09 +0200

    enhanced TextField constructor

--- swing/src/main/scala/dire/swing/TextField.scala
+++ swing/src/main/scala/dire/swing/TextField.scala
@@ -1,7 +1,9 @@
 package dire.swing
 
 import dire._
-import javax.swing.JTextField
+import java.awt.{Color, Font}
+import javax.swing.{JTextField}
+import javax.swing.border.Border
 import scalaz._, Scalaz._, effect.IO
 
 case class TextField(peer: JTextField) extends TextComponent[JTextField] {
@@ -13,8 +15,32 @@ case class TextField(peer: JTextField) extends TextComponent[JTextField] {
 }
 
 object TextField {
-  def apply(text: String = ""): IO[TextField] =
-    IO(TextField(new JTextField(text)))
+  private[this] lazy val dummy = new JTextField
+
+  def apply(text: String = "",
+            background: Color = dummy.getBackground,
+            border: Border = dummy.getBorder,
+            editable: Boolean = true,
+            font: Font = dummy.getFont,
+            foreground: Color = dummy.getForeground,
+            horizontalAlignment: Alignment = Alignment.Leading,
+            opaque: Boolean = dummy.isOpaque,
+            tooltip: Option[String] = None): IO[TextField] = IO {
+    val t = new JTextField(text)
+    t.setBackground(background)
+    t.setBorder(border)
+    t.setEditable(editable)
+    t.setFont(font)
+    t.setForeground(foreground)
+    t.setHorizontalAlignment(horizontalAlignment.v)
+    t.setOpaque(opaque)
+    t.setToolTipText(tooltip getOrElse null)
+
+    TextField(t)
+  }
+
+  def trailing(text: String = ""): IO[TextField] =
+    apply(text, horizontalAlignment = Alignment.Trailing)
 
   implicit val TextFieldElem: AsSingleElem[TextField] = Elem hFill { _.peer }
 
