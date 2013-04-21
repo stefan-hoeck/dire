4eca33335fc3398ca5a8e6a7e6d7bf0b8ab5c581
stefan-hoeck <efasckenoth@gmail.com>
2013-04-21 13:41:55 +0200

    added a HelloWorld example

---
+++ example/src/main/scala/dire/example/HelloWorld.scala
@@ -0,0 +1,26 @@
+package dire.example
+
+import dire._, SF.{id, loop, sfIO} 
+import scalaz._, Scalaz._, effect.IO
+
+object HelloWorld {
+
+  private final val Quit = ":q"
+
+  private def prompt(s: String) =
+    IO.putStrLn(s"Hello ${s}!") >>
+    IO.putStrLn(s"Please enter your name ($Quit to exit).") >>
+    IO.readLn
+
+  def quit = id filter Quit.≟
+
+  def name = id filter Quit.≠ hold "World"
+
+  def hello = loop(name andThen sfIO(prompt))
+
+  def goodbye = name on quit syncTo { s ⇒ IO putStrLn s"Goodbye $s!" }
+
+  def run = SF.run(hello >=> goodbye)(_ ⇒ true)
+}
+
+// vim: set ts=2 sw=2 et:
