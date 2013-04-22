7f7b2f606934b9257d3e605373dd72be59dba255
stefan-hoeck <efasckenoth@gmail.com>
2013-04-22 05:27:59 +0200

    minor adjustments to validated signal functions

--- core/src/main/scala/dire/Validation.scala
+++ core/src/main/scala/dire/Validation.scala
@@ -4,7 +4,33 @@ import scalaz._, Scalaz._, effect.IO
 
 /** Helper functions for dealing with input validation */
 trait ValidationFunctions {
-  import validation.{ValRes, DisRes, Validator, VSIn, SfV, ValidatorIO}
+  import validation.{ValRes, DisRes, Validator, ValidatorIO}
+  type Errors = NonEmptyList[String]
+
+  /** A validated value */
+  type ValRes[+A] = Validation[Errors,A]
+
+  /** A validated value represented as a disjunction */
+  type DisRes[+A] = Errors \/ A
+
+  /** An IO action returning a disjunction */
+  type DisIO[+A] = EitherT[IO,Errors,A]
+
+  /** Reads an input value of type `A` and returns a 
+    * validated value of type `B`.
+    */
+  type Validator[-A,+B] = Kleisli[DisRes,A,B]
+
+  /** Reads an input value of type `A` and returns a 
+    * validated value of type `B` in an IO action.
+    */
+  type ValidatorIO[-A,+B] = Kleisli[DisIO,A,B]
+
+  /** Signal function of a validated value */
+  type SfV[-A,+B] = SF[A,ValRes[B]]
+
+  /** Input signal of a validated value */
+  type VSIn[+A] = SIn[ValRes[A]]
 
   def point[A,B](b: ⇒ B): SfV[A,B] = validation.SfVApplicative[A] point b
 
@@ -15,7 +41,7 @@ trait ValidationFunctions {
     * two validation steps happen sequentially.
     */
   def reValidate[A,B](v: Validator[A,B]): SfV[ValRes[A],B] =
-    SF.id[ValRes[A]] map { _.disjunction flatMap v.run validation }
+    SF.id[ValRes[A]] map { _ @\/ { _ >>= v.run } }
 
   /** Returns a signal faction that turns all input into a success */
   def success[A]: SfV[A,A] = SF.id[A] map { _.success }
@@ -43,33 +69,6 @@ trait ValidationInstances {
     Applicative[({type λ[α]=SF[R,α]})#λ].compose[ValRes]
 }
 
-object validation extends ValidationFunctions with ValidationInstances {
-  type Errors = NonEmptyList[String]
-
-  /** A validated value */
-  type ValRes[+A] = Validation[Errors,A]
-
-  /** A validated value represented as a disjunction */
-  type DisRes[+A] = Errors \/ A
-
-  /** An IO action returning a disjunction */
-  type DisIO[+A] = EitherT[IO,Errors,A]
-
-  /** Reads an input value of type `A` and returns a 
-    * validated value of type `B`.
-    */
-  type Validator[-A,+B] = Kleisli[DisRes,A,B]
-
-  /** Reads an input value of type `A` and returns a 
-    * validated value of type `B` in an IO action.
-    */
-  type ValidatorIO[-A,+B] = Kleisli[DisIO,A,B]
-
-  /** Signal function of a validated value */
-  type SfV[-A,+B] = SF[A,ValRes[B]]
-
-  /** Input signal of a validated value */
-  type VSIn[+A] = SIn[ValRes[A]]
-}
+object validation extends ValidationFunctions with ValidationInstances
 
 // vim: set ts=2 sw=2 et:
