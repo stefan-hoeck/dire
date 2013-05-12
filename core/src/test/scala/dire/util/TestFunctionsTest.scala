98d6e191f1e893d9a0c9cb53b3c77a4162ecf579
stefan-hoeck <efasckenoth@gmail.com>
2013-05-12 06:35:28 +0200

    added tests for dire.test.TestFunctions

---
+++ core/src/test/scala/dire/util/TestFunctionsTest.scala
@@ -0,0 +1,36 @@
+package dire.util
+
+import dire._
+import org.scalacheck._, Prop._
+import scalaz._, Scalaz._
+
+object TestFunctionsTest 
+  extends Properties("TestFunctions")
+  with TestFunctions {
+
+  val hundred = 0 to 100  map { _.toLong } toList
+
+  property("run") = 
+    run(SF.time)(SF sf { _.toString })("100" ≟ _) ≟ hundred
+
+  property("runFor") = runFor(SF.time, 100L) ≟ hundred
+
+  property("runN") = runN(SF.time, 101) ≟ hundred
+
+  property("runUntil") = runUntil(SF.time)(100L <= _) ≟ hundred
+
+  property("runFor_const") = forAll { i: Int ⇒ 
+    runFor(SF const i, 100L) ≟ List(i)
+  }
+
+  property("runN_const") = forAll { i: Int ⇒ 
+    runN(SF const i, 1) ≟ List(i)
+  }
+
+  property("runUntil_const") = forAll { i: Int ⇒ 
+    runUntil(SF const i)(i ≟ _) ≟ List(i)
+  }
+
+}
+
+// vim: set ts=2 sw=2 et:
