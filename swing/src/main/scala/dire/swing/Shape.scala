e060268a05c69790e9a3fd5bb45b04a34cfa5603
Arya Irani <arya.irani@gmail.com>
2014-06-25 12:17:50 -0400

    Added constructor on Shape companion

--- swing/src/main/scala/dire/swing/Shape.scala
+++ swing/src/main/scala/dire/swing/Shape.scala
@@ -9,35 +9,34 @@ trait Shape {
 }
 
 object Shape {
-  lazy val blank: Shape = new Shape {
-    def paint(g: Graphics2D) {}
+  def apply(f: Graphics2D ⇒ Unit): Shape = new Shape {
+    def paint(g: Graphics2D) = f(g)
   }
+
+  lazy val blank: Shape = Shape(_ ⇒ ())
 
   def circle(x: Double, y: Double, r: Double, c: Color): Shape =
     oval(x, y, r, r, c)
 
-  def polyLine(points: IxSeq[Position], c: Color): Shape = new Shape {
+  def polyLine(points: IxSeq[Position], c: Color): Shape = {
     val xs = points map { _._1 } toArray
     val ys = points map { _._2 } toArray
     val size = points.size
 
-    def paint(g: Graphics2D) {
+    Shape { g ⇒
       g.setColor(c)
-
       g.drawPolyline(xs, ys, size)
     }
   }
 
-  def rectangle(x: Double, y: Double, rx: Double, ry: Double, c: Color)
-    : Shape = new Shape {
-      def paint(g: Graphics2D) {
-        g.setColor(c)
-        g.fillRect(x.toInt, y.toInt, rx.toInt, ry.toInt)
-      }
+  def rectangle(x: Double, y: Double, rx: Double, ry: Double, c: Color): Shape =
+    Shape { g ⇒
+      g.setColor(c)
+      g.fillRect(x.toInt, y.toInt, rx.toInt, ry.toInt)
     }
 
   def oval(x: Double, y: Double, rx: Double, ry: Double, c: Color): Shape =
-    new Shape {
+    Shape { g ⇒
       def paint(g: Graphics2D) {
         g.setColor(c)
         g.fillOval(x.toInt, y.toInt, rx.toInt, ry.toInt)
@@ -47,11 +46,9 @@ object Shape {
   def square(x: Double, y: Double, s: Double, c: Color) = 
     rectangle(x, y, s, s, c)
 
-  def combine(a: Shape, b: Shape): Shape  = new Shape {
-    def paint(g: Graphics2D) {
-      a.paint(g)
-      b.paint(g)
-    }
+  def combine(a: Shape, b: Shape): Shape = Shape { g ⇒
+    a.paint(g)
+    b.paint(g)
   }
 
   implicit val ShapeMonoid: Monoid[Shape] = new Monoid[Shape] {
